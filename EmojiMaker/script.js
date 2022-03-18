2fe9bca742723e03f8cc7bdf49b1e0414a318988
Oscar Chavez <oscarchavez121@live.com>
2022-03-18 11:57:46 -0500

    EmojiMaker v2: Array in JavaScript
    Changed it to Array in JavaScript.
    The index.html page holds an input field where user can type a string or number and it will be stored in an array.  4 buttons control insert and delete of array items: begging and end.
    Use of Flex to center CSS

--- EmojiMaker/script.js
+++ EmojiMaker/script.js
@@ -1,7 +1,7 @@
-// database
+// database 🥵
 // TODO: 	- style css
 //			- bound checks when poping or shifting	
-let emojis = ['1', '2', '3']
+let emojis = ['😀', '😐', '😬']
 let outputField = document.getElementById("output")
 let addBegButton = document.getElementById("button1")
 let addEndButton = document.getElementById("button2")
@@ -10,10 +10,21 @@ let input = document.getElementById("input-field")
 let remBegButton = document.getElementById("button3")
 let remEndButton = document.getElementById("button4")
 
+
+let output_ls = document.getElementById("output-local-storage")
+console.log(output_ls)
 function displayEmojis(){
+	if (emojis.length == 0) {
+		outputField.innerHTML = " EMPTY ARRAY "
+	}
 	for (let i = 0; i < emojis.length; i++){
 		console.log(emojis[i])
-		outputField.append(document.createElement('span').innerHTML = emojis[i])
+		let newElement = document.createElement('span')
+		newElement.innerHTML = emojis[i]
+		outputField.append(newElement)
+		//outputField.append(document.createElement('span').innerHTML = emojis[i])
+
+		newElement.className = "array-element"
 	}
 
 }
@@ -59,3 +70,42 @@ remEndButton.addEventListener("click", function(){
 
 // MAIN()
 displayEmojis()
+
+/*
+	localStorage = {key1: value,
+					key2: value,
+					key3: value }
+*/
+
+//localStorage.setItem("key", "value")
+localStorage.setItem("person1", 25)
+localStorage.setItem("person2", 29)
+localStorage.setItem("person3", 5)
+localStorage.setItem("person4", 22)
+
+localStorage.setItem("hot", "🥵")
+console.log("person1 Age: ")
+console.log(localStorage.getItem("person1"))
+console.log("person2 Age: ")
+console.log(localStorage.getItem("person2"))
+console.log("Hot Emoji: ")
+console.log(localStorage.getItem("hot"))
+
+output_ls.innerHTML = localStorage.getItem("person1")
+
+localStorage.clear()
+output_ls.innerHTML = "" 
+
+let LS_count = 0
+localStorage.setItem(0, "person1")
+LS_count += 1
+localStorage.setItem(1, "person2")
+LS_count += 1
+
+
+for (let i = 0; i < LS_count; i++ ){
+	console.log(localStorage.getItem(i))
+	output_ls.append(document.createElement('span').innerHTML = localStorage.getItem(i))
+}
+//let myInformation = localStorage.getItem("key")
+//console.log(myInformation)
